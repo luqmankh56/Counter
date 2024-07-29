c1df103498f8ce98afc10c5d5675d7d099f77ccb
Luqman Khan <158295061+luqmankh56@users.noreply.github.com>
2024-07-30 00:10:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let count = 0;
+let saveEL = document.getElemementById("save-el")
+let countEL = document.getElemementById("count-el")
 
-function increment() {
-  count = count + 1;
-  document.getElementById("count-el").innerText = count;
-  console.log(count);
-}
+  function increment() {
+    count += 1
+    countEl.textContent= count
+  }
 
-function save() {
-  console.log("Count saved:", count);
-}
+  function save() {
+    let countStr = count + " - "
+    saveEl.innerTextStr
+    countEL.textCotent= 0
+    count = 0
+  }
+  console.log("Lets count people on the subway!")
+  
